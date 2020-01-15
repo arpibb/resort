5b78901c9e7b64bd2eae2ab04ea7284211d3d1a2
arpibb <bencze.arpad.21@gmail.com>
2020-01-15 17:46:52 +0100

    first commit

---
+++ src/pages/Error.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Hero from '../components/Hero.js'
+
+export default function Error() {
+    return (
+       <Hero/>
+    )
+}
+
---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Hero from '../components/Hero.js'
+
+export default function Home() {
+    return (
+        <Hero />
+    )
+}
